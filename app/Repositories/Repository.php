fc5b6508cffa191d25817bffac323774527a79a0
Yuri Chandra <yurichandra@gmail.com>
2019-01-13 22:38:35 +0700

    Added class for repository layer

---
+++ app/Repositories/Repository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repositories;
+
+class Repository
+{
+    protected $model;
+
+    public function get()
+    {
+        return $this->model->get();
+    }
+
+    public function find($id)
+    {
+        return $this->model::findOrFail($id);
+    }
+}
---
+++ app/Repositories/ServiceRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repositories;
+
+class ServiceRepository extends Repository
+{
+    protected $model = Service::class;
+}
