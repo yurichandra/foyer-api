3e59e952fc22affdae135da396a9aa810d926941
Yuri Chandra <yurichandra@gmail.com>
2019-01-15 22:08:14 +0700

    Added unit testing for service CRUD

--- tests/RoutingServiceTest.php
+++ tests/RoutingServiceTest.php
@@ -4,9 +4,14 @@ use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
 use App\Services\RoutingService;
 use App\Exceptions\ServiceDuplicationException;
+use App\Models\Service;
+use Faker\Factory;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class RoutingServiceTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
      * A add service test.
      *
@@ -25,6 +30,11 @@ class RoutingServiceTest extends TestCase
         $this->assertTrue($service_created);
     }
 
+    /**
+     * A test to check whether slug unique.
+     *
+     * @return void
+     */
     public function testUniqueSlugWhenAddService()
     {
         $this->expectException(ServiceDuplicationException::class);
@@ -36,5 +46,61 @@ class RoutingServiceTest extends TestCase
             'url' => 'http://127.0.0.1:7000',
             'slug' => 'student.service',
         ]);
+    }
+
+    /**
+     * A test to find service by its slug.
+     *
+     *  @return void
+     */
+    public function testFindServiceBySlug()
+    {
+        $service = new RoutingService();
+        $faker = Factory::create();
+
+        $services_slugs = $service->getServices()
+                            ->map(function ($item) {
+                                return $item->slug;
+                            });
+
+        $service_found = $service->findServiceBySlug($faker->randomElements($services_slugs));
+
+        $this->assertInstanceOf(Service::class, $service_found);
+    }
+
+    /**
+     * A test to update service by its slug.
+     *
+     *  @return void
+     */
+    public function testUpdateService()
+    {
+        $service = new RoutingService();
+        $updated_service = $service->updateService('user.service', ['url' => 'http://127.0.0.1:1000']);
+
+        $this->assertTrue($updated_service);
+    }
+
+    /**
+     * A test to delete service by its slug.
+     *
+     *  @return void
+     */
+    public function testDeleteService()
+    {
+        $this->expectException(ModelNotFoundException::class);
+        $service = new RoutingService();
+        $faker = Factory::create();
+
+        $services_slugs = $service->getServices()
+            ->map(function ($item) {
+                return $item->slug;
+            });
+
+        $slug = $faker->randomElement($services_slugs);
+
+        $service_deleted = $service->deleteService($slug);
+
+        $find_service = $service->findServiceBySlug($slug);
     }
 }
